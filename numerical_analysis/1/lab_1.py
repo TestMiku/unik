c519865bbb9ccef5a29bbbd9b19776febd31a031
MIKU-7437 <nurshanov.dias@gmail.com>
2025-02-04 21:09:35 +0500

    1

--- numerical_analysis/1/lab_1.py
+++ numerical_analysis/1/lab_1.py
@@ -2,15 +2,16 @@ import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import numpy as np
 
-x_min=0
-x_max=2
+x_min, x_max=0, 2
 
 N = 100
-t_max = 1
-dt = 0.02
-a = 1
+
+t, t_max = 0, 1
 
 dx = (x_max-x_min)/(N-1)
+dt = 0.02
+
+a = 1
 c = a * (dt/dx)
 
 if c==1:
@@ -18,32 +19,49 @@ if c==1:
 else:
     print('Число Курента не подходит')
 
-x = np.linspace(x_min, x_max, N)
-u = np.exp(-200 * (x-0.2)**2)
-u_1 = np.zeros(len(u))
+x_coordinates = np.linspace(x_min, x_max, N)
+u_current = np.exp(-200 * (x_coordinates-0.2)**2)
+u_new = np.zeros(len(u_current))
 
-solution_matrix = [u.copy()]
-time_steps = int(t_max/dt)
+solution_matrix = [u_current.copy()]
+num_time_steps = int(t_max/dt)
 
-for n in range(time_steps):
+for step in range(num_time_steps):
     for i in range(1, N-1):
-        u_1[i] = u[i] - c*(u[i] - u[i-1])
+        u_new[i] = u_current[i] - c*(u_current[i] - u_current[i-1])
 
-    u_1[0] = u[0] - c * (u[0] - u[-2])
-    u_1[-1] = u_1[0]
-    u[:] = u_1
-    solution_matrix.append(u_1.copy())
-
-
+    u_new[0] = u_current[0] - c * (u_current[0] - u_current[-2])
+    u_new[-1] = u_current[0]
+    u_current[:] = u_new
+    solution_matrix.append(u_new.copy())
 
 
-fig, axis_x = plt.subplots()
-line, = axis_x.plot(x, solution_matrix[50], color='red')
-axis_x.set_xlim(x_min, x_max)
-axis_x.set_ylim(0, t_max)
-axis_x.set_xlabel('x')
-axis_x.set_xlabel('u(x,t)')
+print(f"u({50}) = {max(solution_matrix[50])}")
 
-axis_x.set_label("perenos equations")
+is_animate = True 
+is_animate = False 
+if is_animate:
+    fig, axis_x = plt.subplots()
+    line, = axis_x.plot(x_coordinates, solution_matrix[0], color='red')
+    axis_x.set_xlim(x_min, x_max)
+    axis_x.set_ylim(0, t_max)
+    axis_x.set_xlabel('x')
+    axis_x.set_xlabel('u(x,t)')
+    axis_x.set_label("perenos equations")
+    def update_plot(frame, x_coordinates, y, line):
+        line.set_ydata(y[frame])
+        return line,
+    ani = animation.FuncAnimation(fig, update_plot, frames=range(len(solution_matrix)), 
+                                  fargs=(x_coordinates, solution_matrix, line), interval=50, blit=True)
+    plt.show()
+else:
+    fig, axis_x = plt.subplots()
+    line, = axis_x.plot(x_coordinates, solution_matrix[50], color='red')
+    axis_x.set_xlim(x_min, x_max)
+    axis_x.set_ylim(0, t_max)
+    axis_x.set_xlabel('x')
+    axis_x.set_xlabel('u(x,t)')
 
-plt.show()
+    axis_x.set_label("perenos equations")
+
+    plt.show()
